a6c90f21f394169de0f8058d9c682cde0044f58e
Yuki Watanabe <ukwhatn@gmail.com>
2025-01-22 18:45:43 +0900

    wip

--- server/util/env.py
+++ server/util/env.py
@@ -3,3 +3,18 @@ import os
 
 def get_env(key, default=None):
     return os.environ.get(key, default)
+
+
+def get_api_keys(env_name: str):
+    raw_keys = os.environ.get(env_name, "").split()
+
+    keys = {}
+
+    for key in raw_keys:
+        if "|" not in key:
+            continue
+
+        app, key = key.split("|")
+        keys[app] = key
+
+    return keys
